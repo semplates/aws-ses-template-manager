b0b00b8c129311ebbb87f2f26635ea17a749df55
Jonathan Mandt <jonathan.mandt@gmail.com>
2023-10-31 17:06:00 +0100

    feat: terraform code for setting up SES and Python Lambda function

---
+++ terraform/ses.tf
@@ -0,0 +1,12 @@
+
+resource "aws_ses_email_identity" "semplates_email_identity" {
+  email = var.verified_email
+}
+
+resource "aws_ses_template" "semplates_demo_template" {
+  name = "SEMPLATES_DEMO_TEMPLATE"
+
+  subject = "Semplates Test Lambda Function"
+  html    = file("${path.module}/static/demo_template.html")
+}
+
---
+++ terraform/variables.tf
@@ -0,0 +1,8 @@
+variable "aws_region" {
+  type = string
+  default = "eu-central-1"
+}
+
+variable "verified_email" {
+  type = string
+}
