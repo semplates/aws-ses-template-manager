d01c9cb7db17eb776c130bb6d8e4be9e18aba685
Jonathan Mandt <jonathan.mandt@gmail.com>
2023-11-03 14:48:59 +0100

    fix: python issue regarding aws region
    Signed-off-by: Jonathan Mandt <jonathan.mandt@gmail.com>

--- lambdas/python/main.py
+++ lambdas/python/main.py
@@ -7,7 +7,7 @@ from botocore.exceptions import ClientError
 # Initialize the SES client.
 # If you're running the function locally, make sure your AWS credentials are set up (e.g., via `~/.aws/credentials`
 # If you're running this on AWS Lambda, ensure the execution role has permissions to use SES.
-ses_client = boto3.client('ses', region_name=os.environ["AWS_REGION"])  # Adjust the region if necessary.
+ses_client = boto3.client('ses')
 
 
 def send_templated_email(receiver_email: str, template_name: str, sender_email: str, template_data: dict):
